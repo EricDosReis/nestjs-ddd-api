c6d58b1592e08f6b10e887b871f88cd0873f7d7c
EricDosReis <ericdosreis.personal@gmail.com>
2024-06-25 16:27:28 -0300

    feat: creates prisma question mapper

--- src/infra/database/prisma/repositories/prisma-questions.ts
+++ src/infra/database/prisma/repositories/prisma-questions.ts
@@ -3,9 +3,13 @@ import { Injectable } from '@nestjs/common';
 import type { Pagination } from '@/core/types/pagination';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions';
 import { Question } from '@/domain/forum/enterprise/entities/question';
+import { PrismaQuestionMapper } from '../mappers/prisma-question';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
   create(question: Question): Promise<void> {
     throw new Error('Method not implemented.');
   }
@@ -18,8 +22,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     throw new Error('Method not implemented.');
   }
 
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.');
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
   findBySlug(slug: string): Promise<Question | null> {
